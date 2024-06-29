84d03956ce4dd2c71ec1d9c62099faf784167d55
Minh Triet <97509338+MinhTrietTran@users.noreply.github.com>
2024-06-29 18:13:51 +0700

    MainBoard UI

--- CareerTide/MainBoardGUI.Designer.cs
+++ CareerTide/MainBoardGUI.Designer.cs
@@ -28,12 +28,211 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainBoardGUI));
+            this.SignInSignUpLB = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LogoPB = new System.Windows.Forms.PictureBox();
+            this.TopDockPB = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.WelcomeLB = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TopDockPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SignInSignUpLB
+            // 
+            this.SignInSignUpLB.AutoSize = true;
+            this.SignInSignUpLB.BackColor = System.Drawing.Color.Black;
+            this.SignInSignUpLB.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SignInSignUpLB.ForeColor = System.Drawing.Color.White;
+            this.SignInSignUpLB.Location = new System.Drawing.Point(934, 9);
+            this.SignInSignUpLB.Name = "SignInSignUpLB";
+            this.SignInSignUpLB.Size = new System.Drawing.Size(183, 27);
+            this.SignInSignUpLB.TabIndex = 3;
+            this.SignInSignUpLB.Text = "Sign in/Sign up";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::CareerTide.Properties.Resources.companies;
+            this.pictureBox3.Location = new System.Drawing.Point(0, 322);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(112, 97);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CareerTide.Properties.Resources.job_find;
+            this.pictureBox2.Location = new System.Drawing.Point(1, 211);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(111, 113);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CareerTide.Properties.Resources.applications_employee;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 103);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(111, 108);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LogoPB
+            // 
+            this.LogoPB.Image = global::CareerTide.Properties.Resources.career_tide_logo;
+            this.LogoPB.Location = new System.Drawing.Point(1, 0);
+            this.LogoPB.Name = "LogoPB";
+            this.LogoPB.Size = new System.Drawing.Size(111, 101);
+            this.LogoPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.LogoPB.TabIndex = 2;
+            this.LogoPB.TabStop = false;
+            // 
+            // TopDockPB
+            // 
+            this.TopDockPB.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopDockPB.Image = global::CareerTide.Properties.Resources.red_and_black_background;
+            this.TopDockPB.Location = new System.Drawing.Point(113, 0);
+            this.TopDockPB.Name = "TopDockPB";
+            this.TopDockPB.Size = new System.Drawing.Size(1014, 100);
+            this.TopDockPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.TopDockPB.TabIndex = 1;
+            this.TopDockPB.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox4.Image = global::CareerTide.Properties.Resources.red_and_black_background;
+            this.pictureBox4.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(113, 587);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            // 
+            // WelcomeLB
+            // 
+            this.WelcomeLB.AutoSize = true;
+            this.WelcomeLB.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WelcomeLB.Location = new System.Drawing.Point(546, 103);
+            this.WelcomeLB.Name = "WelcomeLB";
+            this.WelcomeLB.Size = new System.Drawing.Size(159, 24);
+            this.WelcomeLB.TabIndex = 4;
+            this.WelcomeLB.Text = "Popular companies";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.Location = new System.Drawing.Point(147, 132);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(294, 192);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel2.Location = new System.Drawing.Point(481, 132);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(294, 192);
+            this.panel2.TabIndex = 10;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel3.Location = new System.Drawing.Point(812, 132);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(294, 192);
+            this.panel3.TabIndex = 10;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel4.Location = new System.Drawing.Point(147, 364);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(294, 192);
+            this.panel4.TabIndex = 11;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel5.Location = new System.Drawing.Point(481, 364);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(294, 192);
+            this.panel5.TabIndex = 10;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel6.Location = new System.Drawing.Point(812, 364);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(294, 192);
+            this.panel6.TabIndex = 12;
+            // 
+            // MainBoardGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1127, 587);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.WelcomeLB);
+            this.Controls.Add(this.SignInSignUpLB);
+            this.Controls.Add(this.LogoPB);
+            this.Controls.Add(this.TopDockPB);
+            this.Controls.Add(this.pictureBox4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainBoardGUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainBoardGUI";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TopDockPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox TopDockPB;
+        private System.Windows.Forms.PictureBox LogoPB;
+        private System.Windows.Forms.Label SignInSignUpLB;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label WelcomeLB;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
     }
 }
--- CareerTide/Properties/Resources.Designer.cs
+++ CareerTide/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CareerTide.Properties
-{
-
-
+namespace CareerTide.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,94 @@ namespace CareerTide.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CareerTide.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap applications_employee {
+            get {
+                object obj = ResourceManager.GetObject("applications-employee", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap career_tide_logo {
+            get {
+                object obj = ResourceManager.GetObject("career-tide-logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap companies {
+            get {
+                object obj = ResourceManager.GetObject("companies", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap job_find {
+            get {
+                object obj = ResourceManager.GetObject("job-find", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap red_and_black_background {
+            get {
+                object obj = ResourceManager.GetObject("red-and-black-background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
